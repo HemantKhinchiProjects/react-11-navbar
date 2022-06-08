5824cc48b263f8a93c7ad0391186c63e4a25f3f7
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-06-08 13:37:50 +0530

    1 file modified

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,7 +3,10 @@ const Navbar = () => {
   return (
     <nav>
       <div className="nav-center">
-        <div className="nav-Header"></div>
+        <div className="nav-Header">
+            <i class="fa-solid fa-shield-heart"></i>
+            <button className="nav-toggle"><i class="fa-solid fa-sliders"></i></button>
+        </div>
         <div className="links-container show-container"></div>
         <ul className="social-icons"></ul>
       </div>
