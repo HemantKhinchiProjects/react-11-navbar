d6b7c28008b31c603a9bac19d6ffaf16c14bf05c
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-06-08 14:12:35 +0530

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './style.css';
 import Navbar from './Component/Navbar';
-import Sidebar from './Component/Sidebar';
 export default function App() {
   return (
     <>
       <Navbar />
-      <Sidebar />
     </>
   );
 }
