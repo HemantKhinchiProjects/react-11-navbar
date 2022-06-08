c7593ebd63e470ff2c60e0b15a19816efc6e1b10
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-06-08 13:22:46 +0530

    Initial commit

---
+++ src/Component/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-Header"></div>
+        <div className="links-container show-container"></div>
+        <ul className="social-icons"></ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
